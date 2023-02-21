3e5e7b3f700ec9ecc7886e1b473e7d8d078a5b50
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2023-02-21 22:54:28 +0100

    added register constraints

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,22 +3,57 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('motDePasse')
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'champs vide',
+                    ]),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'champs vide',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please Enter Email',
+                    ]),
+                    new Email(['message' => 'email invalide']),
+                ],
+            ])
+            ->add('motDePasse', PasswordType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please Enter a password',
+                    ]),
+                   
+                ],
+            ])
             ->add('genre', ChoiceType::class, [
                 'choices' => [
                     'User' => 'user',
