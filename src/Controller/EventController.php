cedd9b51a66121f681442002159102638ac41cf1
Ahmedjomaa55 <115115488+Ahmedjomaa55@users.noreply.github.com>
2023-02-22 12:04:12 +0100

    event done

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -7,22 +7,33 @@ use App\Form\EventType;
 use App\Repository\EventsRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class EventController extends AbstractController
 {
-    #[Route('/event/list', name: 'event_list')]
+
+    // FRONT :
+    #[Route('/front/frontlist', name: 'event_list')]
     public function ListEvents(EventsRepository $repository): Response
     {
         $events=$repository->findAll();
-        return $this->render('event/list.html.twig',['events'=>$events]);
+        return $this->render('front/frontlist.html.twig',['events'=>$events]);
+    }
 
+    // list backadmin :
+    #[Route('/event/list', name: 'event_list_back')]
+    public function ListEventsback(EventsRepository $repository): Response
+    {
+        $events=$repository->findAll();
+            return $this->render('event/index.html.twig',['events'=>$events]);
     }
 
 
-    #[Route('/event/add', name: 'event_add')]
+    #[Route('/back/add', name: 'event_add', methods: ['GET', 'POST'])]
     public function addEvent(Request $request,ManagerRegistry $doctrine): Response
     {
         $event=new Events();
@@ -30,47 +41,74 @@ class EventController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $file = $form->get('image')->getData();
+            if ($file instanceof UploadedFile) {
+                $filename = md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move($this->getParameter('uploads'), $filename);
+                $event->setImage($filename); // set image value to $event, not $form->getData()
+            }
+
+
             $entityManager = $doctrine->getManager();
             $entityManager->persist($event);
             $entityManager->flush();
 
-            return $this->redirectToRoute('event_list');
+            return $this->redirectToRoute('event_list_back');
         }
         return $this->render('event/add.html.twig',[
             'form'=> $form->createView(),
         ]);
     }
 
-    #[Route('/event/edit{id}', name: 'event_edit')]
-    public function editEvent(Request $request,Events $events,ManagerRegistry $doctrine): Response
+
+
+
+
+
+    #[Route('modifier/{id}', name: 'modifier')]
+
+    public function modifier(Request $request , ManagerRegistry $doctrine,Events $events): Response
     {
+
         $form = $this->createForm(EventType::class, $events);
         $form->handleRequest($request);
+        if ($form ->IsSubmitted()&& $form->isValid()) {
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $doctrine->getManager();
-            $entityManager->flush();
+            $file = $form->get('image')->getData();
+            if ($file instanceof UploadedFile) {
+                $filename = md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move($this->getParameter('uploads'), $filename);
+                $events->setImage($filename); // set image value to $event, not $form->getData()
+            }
 
-            return $this->render('event/edit.html.twig', [
-                'event' => $events,
-                'form' => $form->createView(),
+
+            $em = $doctrine->getManager();
+            //persist=ajouter
+            $em->persist($events);
+            //flush=pish
+            $em->flush();
+            return $this->redirectToRoute('event_list_back', [
             ]);
         }
+
+        return $this->render('event/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
 
 
-    #[Route('/event/delete{id}', name: 'event_delete')]
-    public function deleteEvent(Request $request,Events $events,ManagerRegistry $doctrine): Response
-    {
-        if($this->isCsrfTokenValid('delete'.$events->getId(),$request->request->get('_token')))
-            //isCsrfTokenValid for security CSRF is a type of security to make sure form cannot be faked by a third party
-        {
-            $entityManager = $doctrine->getManager();
-            $entityManager->remove($events);
-            $entityManager->flush();
-        }
+    #[Route('supprimer/{id}', name: 'supprimer')]
 
-        return $this->redirectToRoute('event_list');
+    public function supprimer($id , ManagerRegistry $doctrine): Response
+    {
+        $em=$doctrine->getManager();
+        $events =$doctrine->getRepository(Events::class);
+        $events =  $events->find($id);
+        $em->remove($events);
+        $em->flush();
+        return $this->redirectToRoute('event_list_back');
+
     }
 
 }
