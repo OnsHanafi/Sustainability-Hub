b2c10abbf518347139c0cab178a2567af582b607
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2023-03-04 12:41:11 +0100

    api Verifying account done

--- src/Controller/UserApiController.php
+++ src/Controller/UserApiController.php
@@ -4,19 +4,25 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserApiController extends AbstractController
 {
     #[Route('/signUp', name: 'app_user_signUpapi')]
-    public function SignUpapi(Request $request, ManagerRegistry $doctrine): Response
+    public function SignUpapi(Request $request, ManagerRegistry $doctrine, MailerInterface $mailer): Response
     {
         //enter user fields
         $nom = $request->query->get("nom");
@@ -44,6 +50,17 @@ class UserApiController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            // Send the activation email to the user
+            $email = (new Email())
+                ->from('Sustainability-Hub@esprit.tn')
+                ->to($user->getEmail())
+                ->subject('Welcome to SustainabilityHub!')
+                ->html($this->renderView('ApiVerifyEmail.html.twig', [
+                    'user' => $user,
+                    'verificationLink' => $this->generateUrl('verify_email_api', ['token' => $verification_token], UrlGeneratorInterface::ABSOLUTE_URL),
+                ]));
+            $mailer->send($email);
+
             //json response
             return new JsonResponse("Account created successfully", Response::HTTP_OK);
         } catch (\Exception $ex) {
@@ -52,9 +69,29 @@ class UserApiController extends AbstractController
         }
     }
 
+    //verifying the email
+    #[Route('/verify_email_api/{token}', name: 'verify_email_api')]
+    public function verifyEmailApi(UserRepository $userRepository, string $token)
+    {
+        $user = $userRepository->findOneBy(['verificationToken' => $token]);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Invalid verification token');
+        }
+
+        // $user->setVerificationToken('null');
+        $user->setIsVerified(true);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response('Account verified successfully , Now you can login');
+    }
+
 
     #[Route('/SignIn', name: 'app_user_SignInapi')]
-    public function SignInapi(Request $request, UserRepository $userRepository, ManagerRegistry $doctrine)
+    public function SignInapi(Request $request, UserRepository $userRepository, ManagerRegistry $doctrine, SessionInterface $session)
     {
         $email = $request->query->get("email");
         $motDePasse = $request->query->get("motDePasse");
@@ -65,22 +102,39 @@ class UserApiController extends AbstractController
         $user = $userRepository->findOneBy(['email' => $email]);
         // if user found 
         if ($user) {
-            // check password
-            if ($motDePasse == $user->getMotDePasse()) {
-                // dump($user);
-                // die;
+            //check User verified
+            if (!($user->isIsVerified())) {
+                return new Response("Verify you email");
+            } else
+                // check password
+                if ($motDePasse === $user->getMotDePasse()) {
 
-                // serialize the object user  
-                $serializer = new Serializer([new ObjectNormalizer()]);
-                $user = $serializer->normalize($user);
-                return new JsonResponse($user);
-            } else {
-                return new JsonResponse("Wrong password");
-            }
+                    // Set user attributes in session
+                    $session->set('user', [
+                        'idUser' => $user->getIdUser(),
+                        'nom' => $user->getNom(),
+                        'prenom' => $user->getPrenom(),
+                        'email' => $user->getEmail(),
+                        'motDePasse' => $user->getMotDePasse(),
+                        'genre' => $user->getGenre(),
+                    ]);
+
+                    // serialize the object user  
+                    $serializer = new Serializer([new ObjectNormalizer()]);
+                    $user = $serializer->normalize($user);
+
+                    return new JsonResponse($user);
+                } else {
+                    return new Response("Wrong password");
+                }
         } else {
-            return new JsonResponse("user doesn't exist or wrong email");
+            return new Response("user doesn't exist or wrong email");
         }
     }
+
+
+
+
     #[Route('/get', name: 'app_user_getAllusers')]
     public function getAllusers(UserRepository $userRepository)
     {
@@ -89,5 +143,44 @@ class UserApiController extends AbstractController
         $serializer = new Serializer([new ObjectNormalizer()]);
         $users = $serializer->normalize($users);
         return new JsonResponse($users);
+    }
+
+    /// update User
+
+    #[Route('/update', name: 'app_user_update_api')]
+    public function editUser(Request $request)
+    {
+        $idUser = $request->query->get("idUser");
+        $nom = $request->query->get("nom");
+        $prenom = $request->query->get("prenom");
+        $email = $request->query->get("email");
+        // $genre = $request->query->get("genre");
+        $motDePasse = $request->query->get("motDePasse");
+        $em = $this->getDoctrine()->getManager();
+        // find the user in the database
+        $user = $em->getRepository(User::class)->find($idUser);
+
+
+        // $user->setCIN($CIN);
+        $user->setNom($nom);
+        $user->setPrenom($prenom);
+        $user->setEmail($email);
+        $user->setMotDePasse($motDePasse);
+
+
+        try {
+            // persist user
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            //serilize user 
+            $serializer = new Serializer([new ObjectNormalizer()]);
+            $user = $serializer->normalize($user);
+
+            return new JsonResponse($user);
+        } catch (\Exception $ex) {
+            return new Response("failed" . $ex->getMessage());
+        }
     }
 }
